9be2e7c8f25337d305f89862c2493fbe3d4238aa
Robosphinx <robosphinx@gmail.com>
2014-06-24 21:55:36 -0400

    Updated versioning and Mod ID.

--- src/main/java/calisbeast/mods/quartz/references/References.java
+++ src/main/java/calisbeast/mods/quartz/references/References.java
@@ -6,9 +6,9 @@ package calisbeast.mods.quartz.references;
 
 public class References {
 	
-	public static final String MODID = "quartz";
+	public static final String MODID = "QuartzPlus";
 	public static final String NAME = "Quartz Plus";
 	public static final String VERSION = "0.8";
-	public static final String BUILD = "0";
+	public static final String BUILD = "3";
 
 }
