2b1b0e2d85fa0f2cc0e3c25fd7dc4d25de393ecd
Robosphinx <robosphinx@gmail.com>
2014-06-26 15:10:17 -0400

    Added config options for Max Y and Chance multiplier for Quartz spawning.

--- src/main/java/calisbeast/mods/quartz/references/QuartzConfig.java
+++ src/main/java/calisbeast/mods/quartz/references/QuartzConfig.java
@@ -17,9 +17,15 @@ public class QuartzConfig {
 		config.load();
 		bool.add(config.get("Block Options", "Quartz Ore Drops Item", true).getBoolean(true));
 		bool.add(config.get("Tools Options", "Quartz Tools Enabled", true).getBoolean(true));
+		doubles.add(config.get("Block Options", "Quartz Spawn Multiplier", 1.0).getDouble(1.0));
+		doubles.add(config.get("Block Options", "Rose Quartz Multiplier", 1.0).getDouble(1.0));
+		doubles.add(config.get("Block Options", "Smoky Quartz Multiplier", 1.0).getDouble(1.0));
+		ints.add(config.get("BlockOptions", "Quartz maximum Y-level to spawn", 50).getInt(50));
 		config.save();
 	}
 	
 	public static ArrayList<Boolean> bool = new ArrayList<Boolean>();
+	public static ArrayList<Double> doubles = new ArrayList<Double>();
+	public static ArrayList<Integer> ints = new ArrayList<Integer>();
 
 }
--- src/main/java/calisbeast/mods/quartz/world/Generation.java
+++ src/main/java/calisbeast/mods/quartz/world/Generation.java
@@ -8,6 +8,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import calisbeast.mods.quartz.Quartz;
+import calisbeast.mods.quartz.references.QuartzConfig;
 import cpw.mods.fml.common.IWorldGenerator;
 
 /**
@@ -16,6 +17,14 @@ import cpw.mods.fml.common.IWorldGenerator;
 
 public class Generation implements IWorldGenerator {
 
+	/*
+	 * This gets our configured multiplier for each ore block. This will be multiplied by our chance variable in spawnOre(args) to give more or less times to spawn the ore. 
+	 */
+	private Double quartzMult = QuartzConfig.doubles.get(0);
+	private Double roseMult = QuartzConfig.doubles.get(1);
+	private Double smokyMult = QuartzConfig.doubles.get(2);
+	private int quartzMaxY = QuartzConfig.ints.get(0);
+	
 	/*
 	 * The actual function that generates/adds the blocks - in the switch, each case corresponds to that dimension number 
 	 * (-1 | Nether, 0 | Overworld, 1 | The End, 7 | Twilight Forest (IF the mod is installed, otherwise case 7 is ignored).
@@ -41,9 +50,9 @@ public class Generation implements IWorldGenerator {
 	 * If you look down to the spawnOre function, All these values correspond to those.
 	 */
 	private void generateWorld(World world, Random random, int x, int z) {
-		this.spawnOre(Quartz.white_quartz_ore, world, random, x, z, 16, 16, 2 + random.nextInt(4), 20, 20, 45, Blocks.stone);
-		this.spawnOre(Quartz.black_quartz_ore, world, random, x, z, 16, 16, 2 + random.nextInt(4), 20, 20, 45, Blocks.stone);
-		this.spawnOre(Quartz.pink_quartz_ore, world, random, x, z, 16, 16, 2 + random.nextInt(4), 20, 20, 45, Blocks.stone);
+		this.spawnOre(Quartz.white_quartz_ore, world, random, x, z, 16, 16, 2 + random.nextInt(6), (int) (20 * quartzMult), 20, quartzMaxY, Blocks.stone);
+		this.spawnOre(Quartz.black_quartz_ore, world, random, x, z, 16, 16, 2 + random.nextInt(6), (int) (20 * smokyMult), 20, quartzMaxY, Blocks.stone);
+		this.spawnOre(Quartz.pink_quartz_ore, world, random, x, z, 16, 16, 2 + random.nextInt(6), (int) (20 * roseMult), 20, quartzMaxY, Blocks.stone);
 	}
 
 	/*
@@ -52,9 +61,9 @@ public class Generation implements IWorldGenerator {
 	 */
 	public void spawnOre(Block blocktospawn, World world, Random random, int BlockX, int BlockZ, int maxX, int maxZ, int VeinSize, int chance, int minY, int maxY, Block blocktoreplace) {
 		for(int i = 0; i < chance; i++){
-			int Xcoord = BlockX + random.nextInt(16);
-			int Zcoord = BlockZ + random.nextInt(16);
-			int Ycoord = random.nextInt(41);
+			int Xcoord = BlockX + random.nextInt(maxX);
+			int Zcoord = BlockZ + random.nextInt(maxZ);
+			int Ycoord = random.nextInt(maxY);
 			(new WorldGenMinable(blocktospawn, VeinSize, blocktoreplace)).generate(world, random, Xcoord, Ycoord, Zcoord);
 		}
 	}
