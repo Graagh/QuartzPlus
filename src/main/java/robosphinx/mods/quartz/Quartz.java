d22d3c304d6dd1c56b9ec1a8c27d53920e8f543d
Robosphinx <robosphinx@gmail.com>
2014-07-16 14:27:57 -0400

    Finished adding Config GUI, but seems non-functional at this point (Except for booleans).

--- src/main/java/robosphinx/mods/quartz/Quartz.java
+++ src/main/java/robosphinx/mods/quartz/Quartz.java
@@ -17,6 +17,7 @@ import robosphinx.mods.quartz.item.QuartzAxe;
 import robosphinx.mods.quartz.item.QuartzPickaxe;
 import robosphinx.mods.quartz.item.QuartzToolMaterial;
 import robosphinx.mods.quartz.reference.Reference;
+import robosphinx.mods.quartz.util.LogHelper;
 import robosphinx.mods.quartz.world.Generation;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -27,6 +28,7 @@ import net.minecraft.item.ItemHoe;
 import net.minecraft.item.ItemSpade;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraftforge.event.world.WorldEvent;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -38,12 +40,11 @@ import cpw.mods.fml.common.registry.GameRegistry;
 /*
  * What is our mod?
  */
-@Mod (modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION/*, guiFactory = Reference.GUI_FACTORY_CLASS*/)
+@Mod (modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class Quartz {
     
     /*
-     * Makes this class accessible from other mods (makes addons and integration
-     * possible).
+     * Makes this class accessible from other mods (makes addons and integration possible).
      */
     @Instance (value = Reference.MOD_ID)
     public static Quartz  instance;
@@ -76,48 +77,43 @@ public class Quartz {
     /*
      * Forge pre-initialization call, most of our loading stuff is here.
      */
-    @SubscribeEvent
-    public void PreInit (FMLPreInitializationEvent event) {
+    @Mod.EventHandler
+    public void preInit (FMLPreInitializationEvent event) {
         
         // Gets the config file: if none exists, one is created.
-        config.init(event.getSuggestedConfigurationFile());
+        ConfigHandler.init(event.getSuggestedConfigurationFile());
         
         // Sets the previously empty block fields above, now the game knows what
         // they are/do.
         setBlocks();
         
         // Defines what rose and smoky quartz items are.
-        smoky_quartz = new Item().setCreativeTab(CreativeTabs.tabMaterials).setUnlocalizedName("black_quartz").setTextureName("quartz:black_quartz");
-        rose_quartz = new Item().setCreativeTab(CreativeTabs.tabMaterials).setUnlocalizedName("pink_quartz").setTextureName("quartz:pink_quartz");
+        smoky_quartz = new Item().setCreativeTab(CreativeTabs.tabMaterials).setUnlocalizedName("smokyQuartz").setTextureName("quartz:black_quartz");
+        rose_quartz = new Item().setCreativeTab(CreativeTabs.tabMaterials).setUnlocalizedName("roseQuartz").setTextureName("quartz:pink_quartz");
         
         // If tools are enabled in the config, add them to the game with
         // crafting recipes.
-        if (config.enableTools) {
-            setTools();
-            registerTools();
-            registerToolRecipes();
-            System.out.println("[INFO] Quartz Plus: Tools enabled.");
-        }
+        
         
         // Adds the items to the registry now that they are defines
         GameRegistry.registerItem(smoky_quartz, smoky_quartz.getUnlocalizedName());
         GameRegistry.registerItem(rose_quartz, rose_quartz.getUnlocalizedName());
         
         // Adds the blocks to the registry now that they are defined.
-        GameRegistry.registerBlock(white_quartz_ore, "white_quartz_ore");
-        GameRegistry.registerBlock(smoky_quartz_ore, "black_quartz_ore");
-        GameRegistry.registerBlock(rose_quartz_ore, "pink_quartz_ore");
-        GameRegistry.registerBlock(smoky_quartz_block, SmokyItemBlock.class, "black_quartz_block");
-        GameRegistry.registerBlock(rose_quartz_block, RoseItemBlock.class, "pink_quartz_block");
+        GameRegistry.registerBlock(white_quartz_ore, "oreQuartzPlus");
+        GameRegistry.registerBlock(smoky_quartz_ore, "oreSmokyQuartz");
+        GameRegistry.registerBlock(rose_quartz_ore, "oreRoseQuartz");
+        GameRegistry.registerBlock(smoky_quartz_block, SmokyItemBlock.class, "blockSmokyQuartz");
+        GameRegistry.registerBlock(rose_quartz_block, RoseItemBlock.class, "blockRoseQuartz");
         
         // GameRegistry.registerBlock(black_quartz_slab, "black_quartz_slab");
         // GameRegistry.registerBlock(double_black_quartz_slab,
         // "double_black_quartz_slab");
-        GameRegistry.registerBlock(smoky_quartz_stairs, "black_quartz_stairs");
-        GameRegistry.registerBlock(rose_quartz_stairs, "pink_quartz_stairs");
+        GameRegistry.registerBlock(smoky_quartz_stairs, "stairsSmokyQuartz");
+        GameRegistry.registerBlock(rose_quartz_stairs, "stairsRoseQuartz");
         
         // Register World Gen
-        GameRegistry.registerWorldGenerator(new Generation(), 0);
+        //GameRegistry.registerWorldGenerator(new Generation(), 0);
         
         // Register Recipes
         registerRecipes();
@@ -126,8 +122,8 @@ public class Quartz {
     /*
      * Forge initialization call, adds smelting to the registry.
      */
-    @SubscribeEvent
-    public void Init (FMLInitializationEvent event) {
+    @Mod.EventHandler
+    public void init (FMLInitializationEvent event) {
         // Smelting
         GameRegistry.addSmelting(Quartz.white_quartz_ore, new ItemStack(Items.quartz), 0.5F);
         GameRegistry.addSmelting(Quartz.smoky_quartz_ore, new ItemStack(Quartz.smoky_quartz), 0.5F);
@@ -135,32 +131,44 @@ public class Quartz {
     }
     
     @SubscribeEvent
-    public void PostInit (FMLPostInitializationEvent event) {
+    public void onWorldLoadedEvent(WorldEvent event) {
+        // Check if the world being loaded is local, then if tools are enabled in the config, add them to the game with
+        // crafting recipes.
+        if (!event.world.isRemote) {
+            if (config.enableTools) {
+                setTools();
+                registerTools();
+                registerToolRecipes();
+                LogHelper.info("Tools enabled");
+            }
+        }
+    }
+    
+    @Mod.EventHandler
+    public void postInit (FMLPostInitializationEvent event) {
         // Stub method at this point - Here if we need it.
-    }
-    
-    /*
-     * Adds info to the blocks, tells the game what they are and what to do with
-     * them.
-     */
-    public void setBlocks () {
-        white_quartz_ore = new QuartzOre(Material.rock).setHardness(4.0F).setResistance(5.0F).setBlockName("oreQuartzPlus").setBlockTextureName("quartz:overworld_quartz_ore");
-        smoky_quartz_ore = new SmokyOre(Material.rock).setHardness(4.0F).setResistance(5.0F).setBlockName("oreBlackQuartz").setBlockTextureName("quartz:black_quartz_ore");
-        rose_quartz_ore = new RoseOre(Material.rock).setHardness(4.0F).setResistance(5.0F).setBlockName("orePinkQuartz").setBlockTextureName("quartz:pink_quartz_ore");
-        smoky_quartz_block = new SmokyQuartz().setStepSound(Block.soundTypePiston).setBlockName("blockBlackQuartz").setBlockTextureName("quartz:black_quartz_block");
-        rose_quartz_block = new RoseQuartz().setStepSound(Block.soundTypePiston).setBlockName("blockPinkQuartz").setBlockTextureName("quartz:pink_quartz_block");
         
-        // double_black_quartz_slab = new
-        // QuartzSlab(true).setHardness(2.0F).setResistance(10.0F).setStepSound(Block.soundTypePiston).setBlockName("blackQuartzSlab");
-        // black_quartz_slab = new
-        // QuartzSlab(false).setHardness(2.0F).setResistance(10.0F).setStepSound(Block.soundTypePiston).setBlockName("blackQuartzSlab");
-        smoky_quartz_stairs = new QuartzStairs(smoky_quartz_block, 0).setBlockName("stairsBlackQuartz");
-        rose_quartz_stairs = new QuartzStairs(rose_quartz_block, 0).setBlockName("stairsPinkQuartz");
+        GameRegistry.registerWorldGenerator(new Generation(), 0);
     }
     
     /*
-     * Adds recipes to the registry for all the quartz blocks and their variants
-     * - Still non-functional).
+     * Adds info to the blocks, tells the game what they are and what to do with them.
+     */
+    public void setBlocks () {        
+        white_quartz_ore = new QuartzOre(Material.rock).setHardness(4.0F).setResistance(5.0F).setBlockName("oreQuartzPlus").setBlockTextureName("quartz:overworld_quartz_ore");
+        smoky_quartz_ore = new SmokyOre(Material.rock).setHardness(4.0F).setResistance(5.0F).setBlockName("oreSmokyQuartz").setBlockTextureName("quartz:black_quartz_ore");
+        rose_quartz_ore = new RoseOre(Material.rock).setHardness(4.0F).setResistance(5.0F).setBlockName("oreRoseQuartz").setBlockTextureName("quartz:pink_quartz_ore");
+        smoky_quartz_block = new SmokyQuartz().setStepSound(Block.soundTypePiston).setBlockName("blockSmokyQuartz").setBlockTextureName("quartz:black_quartz_block");
+        rose_quartz_block = new RoseQuartz().setStepSound(Block.soundTypePiston).setBlockName("blockRoseQuartz").setBlockTextureName("quartz:pink_quartz_block");
+        
+        // double_black_quartz_slab = new QuartzSlab(true).setHardness(2.0F).setResistance(10.0F).setStepSound(Block.soundTypePiston).setBlockName("blackQuartzSlab");
+        // black_quartz_slab = new QuartzSlab(false).setHardness(2.0F).setResistance(10.0F).setStepSound(Block.soundTypePiston).setBlockName("blackQuartzSlab");
+        smoky_quartz_stairs = new QuartzStairs(smoky_quartz_block, 0).setBlockName("stairsSmokyQuartz");
+        rose_quartz_stairs = new QuartzStairs(rose_quartz_block, 0).setBlockName("stairsRoseQuartz");
+    }
+    
+    /*
+     * Adds recipes to the registry for all the quartz blocks and their variants - Still non-functional).
      */
     public void registerRecipes () {
         // Commented out because of non-functionality.
@@ -197,13 +205,14 @@ public class Quartz {
         
         // Quartz used in crafting (just makes writing the recipes faster).
         ItemStack quartzStack = new ItemStack(Items.quartz);
+        ItemStack stick = new ItemStack(Items.stick);
         
         // Adds recipes to the registry for Quartz Tools.
-        GameRegistry.addRecipe(new ItemStack(Quartz.quartz_axe), "xx", "x#", " #", 'x', quartzStack, '#', Items.stick);
-        GameRegistry.addRecipe(new ItemStack(Quartz.quartz_hoe), "xx", " #", " #", 'x', quartzStack, '#', Items.stick);
-        GameRegistry.addRecipe(new ItemStack(Quartz.quartz_pickaxe), "xxx", " # ", " # ", 'x', quartzStack, '#', Items.stick);
-        GameRegistry.addRecipe(new ItemStack(Quartz.quartz_shovel), "x", "#", "#", 'x', quartzStack, '#', Items.stick);
-        GameRegistry.addRecipe(new ItemStack(Quartz.quartz_sword), "x", "x", "#", 'x', quartzStack, '#', Items.stick);
+        GameRegistry.addRecipe(new ItemStack(Quartz.quartz_axe), "xx", "x#", " #", 'x', quartzStack, '#', stick);
+        GameRegistry.addRecipe(new ItemStack(Quartz.quartz_hoe), "xx", " #", " #", 'x', quartzStack, '#', stick);
+        GameRegistry.addRecipe(new ItemStack(Quartz.quartz_pickaxe), "xxx", " # ", " # ", 'x', quartzStack, '#', stick);
+        GameRegistry.addRecipe(new ItemStack(Quartz.quartz_shovel), "x", "#", "#", 'x', quartzStack, '#', stick);
+        GameRegistry.addRecipe(new ItemStack(Quartz.quartz_sword), "x", "x", "#", 'x', quartzStack, '#', stick);
         
     }
 }
